122577d5703d2d8e71764bd726e39bc0aab519f2
Rohit Beniwal <96646591+RohitBeniwal@users.noreply.github.com>
2024-02-03 15:25:37 +0530

    Add files via upload

---
+++ express-api/app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const port=3001;
+app.use(cors());
+app.get('/api/data',(req,res)=>{
+    const data=require('./data.json');
+    res.json(data);
+});
+
+app.listen(port,()=>{
+    console.log('Server running');
+});
---
+++ react-app/src/App.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+const App=()=>{
+  const [students,setStudents]=useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:3001/api/data').then(response=>setStudents(response.data)).catch(error=>console.error('Error fetching data:',error));
+
+  },[]);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student=>(
+            <tr key={student.id}>
+              <td>{student.id}</td>
+              <td>{student.first_name}</td>
+              <td>{student.last_name}</td>
+              <td>{student.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+export default App;
